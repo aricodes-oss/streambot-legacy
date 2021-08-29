3879a3464af5f02b2a687e1bc6a4bbd1a491202d
Aria Taylor <ari@aricodes.net>
2021-08-28 22:54:57 -0400

    Better flush

--- streambot/commands/flush.py
+++ streambot/commands/flush.py
@@ -1,16 +1,16 @@
 import asyncio
 
+from streambot import env
 from streambot.db import Reservation, Stream
 from streambot.discord import client
 
 from discord import Object
 
-from contextlib import suppress
-
 
 async def _work():
+    await client.login(env("DISCORD_BOT_TOKEN"))
     for reservation in Reservation.select().prefetch(Stream):
-        with suppress(Exception):
+        try:
             guild = client.get_guild(reservation.guild_id)
             channel = guild.get_channel(reservation.channel_id)
 
@@ -22,6 +22,8 @@ async def _work():
                 await channel.delete_messages(messages_to_delete)
 
             print(f"Deleted {len(messages_to_delete)} messages")
+        except Exception as e:
+            print(e)
 
         for stream in reservation.streams:
             stream.delete_instance()
