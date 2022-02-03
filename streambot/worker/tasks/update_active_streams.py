ce562b9313c518101beb273f30d8fa05b1c73ba2
Aria Taylor <ari@aricodes.net>
2022-02-02 19:10:30 -0500

    Add some de-duplicating logic for enormous requests

--- streambot/worker/tasks/update_active_streams.py
+++ streambot/worker/tasks/update_active_streams.py
@@ -66,8 +66,20 @@ async def _update(reservation):
         stream for stream in live_streams if stream["user_login"] not in known_usernames
     ]
 
+    # Discord limits us to 5 outgoing messages at once
+    # and then sleeps for a second. If we're posting a HUGELY
+    # popular game, then it's possible that we sleep while posting
+    # and the list updates underneath our feet.
+    needs_mid_refresh = len(new_streams) > 5
+    posted = 0
+
     for stream in new_streams:
+        # See previous note about potentially needing mid-refresh
+        if stream["user_login"] in known_usernames:
+            continue
+
         message = await channel.send(embed=_embed(stream))
+        posted += 1
 
         try:
             Stream.create(
@@ -77,6 +89,10 @@ async def _update(reservation):
             )
         except Exception:
             await message.delete()
+
+        if needs_mid_refresh and posted % 3 == 0:
+            streams = Stream.select(reservation=reservation)
+            known_usernames = {s.username for s in streams}
 
 
 async def run():
